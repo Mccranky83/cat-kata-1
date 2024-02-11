75a97003d25204920f0fd3faf4da3dde760e0de0
Mccranky83 <ericcai20021118@gmail.com>
2024-02-11 16:14:40 +0800

    feat: Add server initialization script

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.join(path.dirname(__filename), "/");
+const publicPath = path.join(__dirname, "../dist/");
+const portNumber = process.env.PORT || 8537;
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(`${publicPath}`, "index.html"));
+});
+
+app.listen(portNumber, () => {
+  console.log(`App started on port http://localhost:${portNumber}...`);
+});
